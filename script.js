660f4e96898cf01036adb636ed3768f7e251d56c
luis <luis.moreno@exatec.tec.mx>
2023-12-16 16:12:55 -0600

    Added: letters, numbers, and symbols

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
 // Retrieve document elements
 let genPwdBtn = document.querySelector("#password-btn");
 let firstPwdEl = document.querySelector("#password-1");
