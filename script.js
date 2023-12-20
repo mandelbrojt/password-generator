7fa24748d4ec7e854c6483e9a33b6261e5eb53a4
luis <luis.moreno@exatec.tec.mx>
2023-12-19 23:19:42 -0600

    Added: random password generator hardcoded to 15 letters (upper and lower case)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
@@ -16,9 +15,19 @@ const numbersEnabled = document.querySelector("#toggle-nums").checked;
 // Check input values
 
 // Define functions
+function randomIndex(charsArray) {
+    return Math.floor(Math.random() * charsArray.length)
+}
+
 function generatePasswords() {
-    let message = `Generating passwords with: ${pwdLength} and ${symbolsEnabled} and ${numbersEnabled}`;
-    alert(message);
+    let randomPassOne = "";
+    let randomPassTwo = "";
+    for (let i=0; i < pwdLength; i++) {
+        randomPassOne += letters[randomIndex(letters)];
+        randomPassTwo += letters[randomIndex(letters)];
+    }
+    firstPwdEl.textContent = randomPassOne;
+    secondPwdEl.textContent = randomPassTwo;
 }
 
 function copyToClipboard(passwordId) {
