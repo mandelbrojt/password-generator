2e4c47e6ed5e7973fd7f6d5cdd95b9cd477dc031
luis <luis.moreno@exatec.tec.mx>
2023-12-20 09:56:09 -0600

    Added: concatenated chars array depending on user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
@@ -7,25 +8,38 @@ let genPwdBtn = document.querySelector("#password-btn");
 let firstPwdEl = document.querySelector("#password-1");
 let secondPwdEl = document.querySelector("#password-2");
 
-// Retrieve user input values and states
-const pwdLength = document.querySelector("#pwd-length").value;
-const symbolsEnabled = document.querySelector("#toggle-syms").checked;
-const numbersEnabled = document.querySelector("#toggle-nums").checked;
-
-// Check input values
-
 // Define functions
 function randomIndex(charsArray) {
     return Math.floor(Math.random() * charsArray.length)
 }
 
 function generatePasswords() {
+    // Retrieve user input values and states
+    const pwdLength = document.querySelector("#pwd-length").value;
+    const upperCaseEnabled = document.querySelector("#toggle-uppercase").checked;
+    const symbolsEnabled = document.querySelector("#toggle-syms").checked;
+    const numbersEnabled = document.querySelector("#toggle-nums").checked;
+    
     let randomPassOne = "";
     let randomPassTwo = "";
-    for (let i=0; i < pwdLength; i++) {
-        randomPassOne += letters[randomIndex(letters)];
-        randomPassTwo += letters[randomIndex(letters)];
+    let chars = lowerLetters;
+
+    // Evaluate user input to include special characters
+    if (upperCaseEnabled) {
+        chars = chars.concat(upperLetters);
     }
+    if (symbolsEnabled) {
+        chars = chars.concat(symbols);
+    }
+    if (numbersEnabled) {
+        chars = chars.concat(numbers);
+    }
+    
+    for (let i=0; i < pwdLength; i++) {
+        randomPassOne += chars[randomIndex(chars)];
+        randomPassTwo += chars[randomIndex(chars)];
+    }
+    
     firstPwdEl.textContent = randomPassOne;
     secondPwdEl.textContent = randomPassTwo;
 }
